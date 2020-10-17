982851b98b3cc54baa2d9c06dc7a86c52561908b
omar <omaraser93@gmail.com>
2020-10-17 16:00:54 +0200

    Updates

---
+++ precache-manifest.5652f1ef7a6a45571a8e62d77d83373d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1430962cd84493ca1646cca5071cbc",
+    "url": "/Movies-Overview/index.html"
+  },
+  {
+    "revision": "17a07e991416ae248a05",
+    "url": "/Movies-Overview/static/css/main.7fad5a40.chunk.css"
+  },
+  {
+    "revision": "3f71be5fe7c253ded088",
+    "url": "/Movies-Overview/static/js/2.4d1f9be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-Overview/static/js/2.4d1f9be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a07e991416ae248a05",
+    "url": "/Movies-Overview/static/js/main.05ddb8ca.chunk.js"
+  },
+  {
+    "revision": "f2cb39742b1f09a4fff3",
+    "url": "/Movies-Overview/static/js/runtime-main.7c3b5c11.js"
+  }
+]);
